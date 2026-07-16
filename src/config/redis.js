ee47ac9dffa72554b333b33f438536e4a12ff060
sagargoel072005 <goels4280@gmail.com>
2026-07-16 14:47:04 +0530

    redis docker setup

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,18 +1,14 @@
 const IORedis = require("ioredis");
 
 const redis = new IORedis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
-  maxRetriesPerRequest: null,
-  tls: {}
-});
-redis.on("connect", () => {
-  console.log("Redis Connected");
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD || undefined,
+  maxRetriesPerRequest: null, 
 });
 
 redis.on("error", (err) => {
-  console.log("Redis Error:", err.message);
+  console.error("Redis connection error:", err.message);
 });
 
 module.exports = redis;
