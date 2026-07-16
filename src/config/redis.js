24b56d2c3de68210fe594117db32b6eb4d8e7d1c
sagargoel072005 <goels4280@gmail.com>
2026-07-16 18:34:23 +0530

    transcript bugs fixed

--- src/config/redis.js
+++ src/config/redis.js
@@ -12,3 +12,15 @@ redis.on("error", (err) => {
 });
 
 module.exports = redis;
+
+// const IORedis = require("ioredis");
+
+// const redis = new IORedis({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT,
+//   password: process.env.REDIS_PASSWORD,
+//   maxRetriesPerRequest: null,
+//   tls: {}
+// });
+
+// module.exports = redis;
